d7702f1d0619042d9766cd2329193bee3dc3088a
zardoz03 <zardoz030@gmail.com>
2021-11-12 00:05:00 +0000

    formatting bs out of spite

--- filled_ellipse.scm
+++ filled_ellipse.scm
@@ -1,30 +1,30 @@
-(define
- (draw-an-ellipse image drawable radius)
- (define x (car (cdr (gimp-selection-bounds image))))
- (define y (car (cdr (cdr (gimp-selection-bounds image)))))
- (define width (- (car (cdr (cdr (cdr (gimp-selection-bounds image))))) x))
- (define height (- (car (cdr (cdr (cdr (cdr (gimp-selection-bounds image)))))) y))
- (gimp-context-push)
- (gimp-image-select-ellipse image 2 x y width height)
- (gimp-selection-border image radius)
- (gimp-drawable-edit-fill drawable 0)
- (gimp-selection-none image)
- (gimp-displays-flush)
- (gimp-context-pop)
-)
+(define (draw-an-ellipse image drawable radius)
+  (letrec ([img-bnds (gimp-selection-bounds image)]
+           [ofIMG (lambda (x) (list-ref img-bnds x))]
+           [x (ofIMG 1)]
+           [y (ofIMG 2)]
+           [width  (- (ofIMG 3)
+                      x)]
+           [height (- (ofIMG 4)
+                      y)])
+    (gimp-context-push)
 
+    (gimp-image-select-ellipse image 2 x y width height)
+    (gimp-selection-border image radius)
+    (gimp-drawable-edit-fill drawable 0)
+    (gimp-selection-none image)
+    (gimp-displays-flush)
 
+    (gimp-context-pop)))
 
 (script-fu-register "draw-an-ellipse"
                     "Draw Ellipse"
-                     "Creates a hollow ellipse"
-                     "Lorena"
-                     "Copyright 2021, Lorena"
-                     "30 May 2021"
-                     "" 
-                     SF-IMAGE      "Image"                          1
-                     SF-DRAWABLE   "Layer"                          1
-                     SF-VALUE      "Perimeter Outline Radius"      "5"
-                     )
-
+                    "Creates a hollow ellipse"
+                    "Lorena"
+                    "Copyright 2021, Lorena"
+                    "30 May 2021"
+                    ""
+                    SF-IMAGE      "Image"                          1
+                    SF-DRAWABLE   "Layer"                          1
+                    SF-VALUE      "Perimeter Outline Radius"      "5")
 (script-fu-menu-register "draw-an-ellipse" "<Image>/Edit/Create")
